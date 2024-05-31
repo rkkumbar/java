f215585db63877ea404c281cba00bdd310683aa5
rkkumbar <rkkumbar97@gmail.com>
2024-05-31 01:53:09 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator{
+
+
+  public void addition(int a, int b){
+	 int sum=a+b;
+System.out.println("addition="+sum);	 
+  }
+  
+  public void addition(int a, int b,int c){
+	  
+	  int sum=a+b+c;  
+	  System.out.println("addition="+sum);
+	 
+  }
+  
+  public void subtraction(int a, int b){
+	 int sub=a-b;
+System.out.println("subtraction"+sub);	 
+  }
+}
